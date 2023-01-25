bcefc9f7097dfb222f1f57db76c6b9e326174170
Attila Starkenius <symphtom@hotmail.com>
2023-01-25 17:00:16 +0100

    Add project files.

---
+++ Övning14/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Övning14.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Övning14/Models/GymClass.cs
@@ -0,0 +1,12 @@
+﻿namespace Övning14.Models
+{
+    public class GymClass
+    {
+       public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime EndTime { get { return StartTime + Duration; } }
+        public String Description { get; set; }
+    }
+}
